714f204cae7866e83b6df904ed9172a7d4566180
Edson0012 <Edsonewertonrs1@gmail.com>
2022-09-16 14:50:15 -0300

    finalizado rota de registro com forms

--- src/components/signup/SignUp.js
+++ src/components/signup/SignUp.js
@@ -1,24 +1,99 @@
 import styled from "styled-components";
 import { useNavigate } from "react-router-dom";
 import { useState, useContext } from "react";
+import axios from "axios";
+import { ThreeDots } from "react-loader-spinner";
 
 export default function SignUp(){
     const navigate = useNavigate();
+    const [loading, setLoading] = useState("Cadastrar");
+    const [password2, setPassword2] = useState('')
+    const [form, setForm] = useState({
+        name: "",
+        email: "",
+        password: "",
+        cep: "",
+        rdn: "",
+        phone: ""
+    })
+    const [block, setBlock] = useState(false)
+
+    function changeSubmit(event) {
+        event.preventDefault();
+
+        setForm({
+            ...form,
+            [event.target.name]: event.target.value,
+        });
+    }
+
+    function handleSubmit(event) {
+        event.preventDefault();
+       
+        if(
+            form.name === '' ||
+            form.email === "" ||
+            form.password === "" ||
+            password2 === "" ||
+            form.cep === "" ||
+            form.rdn === "" ||
+            form.phone === ""
+        ){
+            alert('preencha todos os campos')
+            return
+        } else if (form.password.length <= 6 ) {
+            alert('a senha deve ter pelomenos 6 digitos')
+            return
+        } else {
+        } 
+
+        if(form.password === password2){
+            setLoading(<ThreeDots color="white" width="30px" height="15px" />);
+
+            const body = {
+                name: form.name,
+                email: form.email,
+                password: form.password,
+                cep: form.cep,
+                rdn: form.rdn,
+                phone: form.phone
+            };
+
+            axios.post(
+                "http://localhost:5000/sign-up",
+                body
+            ).then((res) => {
+                setBlock(true);
+                navigate("/sign-in");
+            }).catch((err) => {
+                setBlock(true);
+                setLoading('Cadastrar');
+                setBlock(false);
+            });
+        } else {
+            alert('as senhas devem coincidirem')
+            return
+        }
+       
+    }
+
 
     return (
         <Main>
             <h1>GeekDriven Store</h1>
-            <form>
-                <input type='text' placeholder="Nome"  ></input>
-                <input type="email" placeholder="E-mail" ></input>
-                <input type="password" placeholder="Senha" ></input>
-                <input type="password" placeholder="Confirme a senha" ></input>
-                <input type="number" placeholder="CEP" ></input>
-                <input type="text" placeholder="Nº da residencia" ></input>
-                <input type="number" placeholder="Telefone"></input>
-                <button onClick={() => navigate('/sign-in')} > Cadastrar </button>
+            <form onSubmit={handleSubmit} >
+                <input type='text' placeholder="Nome" name="name" value={form.name} onChange={changeSubmit} disabled={block} ></input>
+                <input type="email" placeholder="E-mail" name="email" value={form.email} onChange={changeSubmit} disabled={block}></input>
+                <input type="password" placeholder="Senha" name="password" value={form.password} onChange={changeSubmit} disabled={block} ></input>
+                <input type="password" placeholder="Confirme a senha" name="password2" 
+                value={password2} onChange={(e) => setPassword2(e.target.value)} disabled={block}
+                ></input>
+                <input type="number" placeholder="CEP"  name="cep" value={form.cep}  onChange={changeSubmit} disabled={block} ></input>
+                <input type="text" placeholder="Nº da residencia" name="rdn" value={form.rdn} onChange={changeSubmit} disabled={block} ></input>
+                <input type="number" placeholder="DDD-Telefone" name="phone" value={form.phone} onChange={changeSubmit} disabled={block} ></input>
+                <button type="submit" > {loading} </button>
             </form>
-            <h3> ja possui uma conta? faça o login!.</h3>
+            <h3 onClick={() => navigate('/sign-in')} > ja possui uma conta? faça o login!.</h3>
         </Main>
     )
 }
@@ -78,6 +153,10 @@ gap: 15px;
                 font-size: 15px;
 
                 color: #ffffff;
+
+                display: flex;
+                align-items: center;
+                justify-content: center;
             }
         }
 
